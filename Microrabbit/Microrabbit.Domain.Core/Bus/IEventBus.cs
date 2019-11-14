07694ec5af421d021adebb2c79c6d7cade43b278
14218 <14218@COMPND-DTP-203>
2019-11-14 11:44:33 -0800

    Implement interface

---
+++ Microrabbit/Microrabbit.Domain.Core/Bus/IEventBus.cs
@@ -0,0 +1,19 @@
+﻿using Microrabbit.Domain.Core.Commands;
+using Microrabbit.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microrabbit.Domain.Core.Bus
+{
+   public interface IEventBus 
+    {
+        Task SendCommand<T>(T command) where T : Command;
+        Task Publish<T>(T @event) where T : Event;
+        Task Subscribe<T, TH>()
+            where T : Event
+            where TH : IEventHandler<T>;
+        
+    }
+}
---
+++ Microrabbit/Microrabbit.Domain.Core/Events/Event.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microrabbit.Domain.Core.Events
+{
+   public abstract class Event
+    {
+        public DateTime Timestamp { get; protected set; }
+
+        protected Event()
+        {
+            Timestamp = DateTime.Now;
+        }
+    }
+}
