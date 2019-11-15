482700e950ad13c518c1a5677f5fa90b8a66ca79
14218 <14218@COMPND-DTP-203>
2019-11-14 18:42:20 -0800

    Domain Layer commited

---
+++ Microrabbit/RabbitMq.Banking.Domain/Models/Account.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RabbitMq.Banking.Domain.Models
+{
+  public  class Account 
+    {
+        public int AccountId { get; set; }
+        public string AccountType { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
